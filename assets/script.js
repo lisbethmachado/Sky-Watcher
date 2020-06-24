354e370bcffca20eacf6101f3fcbfacceb548836
Lisbeth Machado <lisbeth@MacBook-Pro.local>
2020-06-23 23:30:00 -0700

    added jquery

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+//     var travels = []
+
+//     for (var i = 0; i < travels.length; i++) {
+//         value = travels[i];
+//         console.log(value);
+//         $(value).html("#forecast");
+//     }
+//     $("#forecast").html("<h1>" + response.name + " Weather Details</h1>");
+
+//     $("#forecast").html(JSON.stringify(response));
+//     $("#forecast").html(JSON.parse(response));
+// });
+
+
+// $("#forecast").prepend(value);
